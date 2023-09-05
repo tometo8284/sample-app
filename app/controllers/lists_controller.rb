92be3391e13cc11193631f06d95fa33abbb12c51
EC2 Default User <ec2-user@ip-172-31-14-67.ap-northeast-1.compute.internal>
2023-09-05 07:20:04 +0000

    firstcommit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,47 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all.order("created_at desc")
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(@list.id)
+    else 
+      redirect_to lists_path
+    end
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path
+  end  
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ app/models/list.rb
@@ -0,0 +1,14 @@
+class List < ApplicationRecord
+  has_one_attached :image
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_fhoto.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [500, 500]).processed
+  end
+end
